cdcb6f1ab3b9a47d7aa7a630ca247874c4656a1a
Admin <perchey.loic@gmail.com>
2022-03-03 13:28:19 +0100

    Initial commit

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -6,33 +6,32 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/series", name="serie_")
+ */
 class SerieController extends AbstractController
 {
     /**
-     * @Route("/series", name="serie_list")
+     * @Route("", name="list")
      */
     public function list(): Response
     {
         return $this->render('serie/list.html.twig', [
-
         ]);
     }
-
     /**
-     * @Route("/series/details/{id}", name="serie_details")
+     * @Route("/series/details", name="details")
      */
     public function details(int $id): Response
     {
         return $this->render('serie/details.html.twig', [
         ]);
     }
-
     /**
      * @Route("/create", name="create")
      */
-    public function create(): Response
+    public function details(int $id): Response
     {
-        dump("yo");
         return $this->render('serie/create.html.twig', [
         ]);
     }
